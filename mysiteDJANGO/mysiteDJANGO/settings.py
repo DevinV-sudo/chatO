46dac564fb375fe346e801917a2c957be280695d
devin.vliet <DevinV08@gmail.com>
2024-11-19 12:35:24 -0800

    whisper-generated-transcription: background-tasks

--- mysiteDJANGO/mysiteDJANGO/settings.py
+++ mysiteDJANGO/mysiteDJANGO/settings.py
@@ -15,7 +15,7 @@ import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+print(BASE_DIR)
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
